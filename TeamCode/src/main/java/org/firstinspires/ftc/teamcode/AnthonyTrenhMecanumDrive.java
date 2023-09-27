b72276ce1fd9b0c140620388e746071916ce6dad
anthonytrenh <trenhanthony@gmail.com>
2023-09-26 17:19:39 -0700

    swapped motor directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnthonyTrenhMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnthonyTrenhMecanumDrive.java
@@ -81,10 +81,10 @@ public class AnthonyTrenhMecanumDrive extends LinearOpMode {
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         servo.setDirection(Servo.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
