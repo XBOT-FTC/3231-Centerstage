e2e4d32bc623ed8bf0b9542983da8af9b44ac625
ImBadAtCS <jiaquanisagod@gmail.com>
2023-10-26 18:08:17 -0700

    Three Rectangles Drawn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/SamplePipeLine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/SamplePipeLine.java
@@ -14,11 +14,15 @@ import org.opencv.core.Rect;
 
 public class SamplePipeLine implements VisionProcessor {
 
-    public Rect rect = new Rect(20, 20, 50, 50);
+    public Rect rectLeft = new Rect(150, 220, 65, 65);
+    public Rect rectMiddle = new Rect(280, 220, 65, 65);
+    public Rect rectRight = new Rect(410, 220, 65, 65);
+    Selected selection = Selected.NONE;
 
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
         // Code executed on the first frame dispatched into this VisionProcessor
+
     }
 
     @Override
@@ -28,20 +32,6 @@ public class SamplePipeLine implements VisionProcessor {
         return frame;
     }
 
-    @Override
-    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-        // Cool feature: This method is used for drawing annotations onto
-        // the displayed image, e.g outlining and indicating which objects
-        // are being detected on the screen, using a GPU and high quality
-        // graphics Canvas which allow for crisp quality shapes.
-        Paint rectPaint = new Paint();
-        rectPaint.setColor(Color.GREEN);
-        rectPaint.setStyle(Paint.Style.STROKE);
-        rectPaint.setStrokeWidth(scaleCanvasDensity * 4);
-
-        canvas.drawRect(makeGraphicsRect(rect, scaleBmpPxToCanvasPx), rectPaint);
-    }
-
     private android.graphics.Rect makeGraphicsRect(Rect rect, float scaleBmpPxToCanvasPx) {
         int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
         int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
@@ -49,6 +39,56 @@ public class SamplePipeLine implements VisionProcessor {
         int bottom = top + Math.round(rect.height * scaleBmpPxToCanvasPx);
 
         return new android.graphics.Rect(left, top, right, bottom);
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        // Cool feature: This method is used for drawing annotations onto
+        // the displayed image, e.g outlining and indicating which objects
+        // are being detected on the screen, using a GPU and high quality
+        // graphics Canvas which allow for crisp quality shapes.
+        Paint selectedPaint = new Paint();
+        selectedPaint.setColor(Color.RED);
+        selectedPaint.setStyle(Paint.Style.STROKE);
+        selectedPaint.setStrokeWidth(scaleCanvasDensity * 4);
+
+        Paint nonSelectedPaint = new Paint(selectedPaint);
+        nonSelectedPaint.setColor(Color.GREEN);
+
+        android.graphics.Rect drawRectangleLeft = makeGraphicsRect(rectLeft, scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleMiddle = makeGraphicsRect(rectMiddle, scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleRight = makeGraphicsRect(rectRight, scaleBmpPxToCanvasPx);
+
+        switch (selection) {
+            case LEFT:
+                canvas.drawRect(drawRectangleLeft, selectedPaint);
+                canvas.drawRect(drawRectangleMiddle, nonSelectedPaint);
+                canvas.drawRect(drawRectangleRight, nonSelectedPaint);
+                break;
+            case MIDDLE:
+                canvas.drawRect(drawRectangleLeft, nonSelectedPaint);
+                canvas.drawRect(drawRectangleMiddle, selectedPaint);
+                canvas.drawRect(drawRectangleRight, nonSelectedPaint);
+                break;
+            case RIGHT:
+                canvas.drawRect(drawRectangleLeft, nonSelectedPaint);
+                canvas.drawRect(drawRectangleMiddle, nonSelectedPaint);
+                canvas.drawRect(drawRectangleRight, selectedPaint);
+                break;
+            case NONE:
+                canvas.drawRect(drawRectangleLeft, nonSelectedPaint);
+                canvas.drawRect(drawRectangleMiddle, nonSelectedPaint);
+                canvas.drawRect(drawRectangleRight, nonSelectedPaint);
+                break;
+
+        }
+    }
+
+    public enum Selected {
+        NONE,
+        LEFT,
+        MIDDLE,
+        RIGHT
     }
 
     }
