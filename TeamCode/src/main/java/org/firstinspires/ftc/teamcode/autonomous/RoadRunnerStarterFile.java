a801706ad1038d630ffee4862be5754ab3f4c2ff
teamxbot-27 <teamxbot-27@outlook.com>
2023-10-31 18:25:13 -0700

    renamed MeepMeep to RoadRunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RoadRunnerStarterFile.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "Basic MeepMeep Autonomous", group = "Autonomous")
+public class RoadRunnerStarterFile extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0,0,0);
+    }
+}
