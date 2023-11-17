43d67f0ff50ce59a74388a675a5a527edd294f6d
ImBadAtCS <jiaquanisagod@gmail.com>
2023-11-16 18:22:31 -0800

    added auto for three rectangles

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/ThreeRectangleAutoTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+public class ThreeRectangleAutoTest extends LinearOpMode {
+
+    VisionPortal myVisionPortal;
+    ThreeRectangleProcessor threeRectangleProcessor;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        VisionPortal.Builder myVisionPortalBuilder;
+        VisionPortal myVisionPortal;
+
+        // Create a new VisionPortal Builder object.
+        myVisionPortalBuilder = new VisionPortal.Builder();
+
+        // Specify the camera to be used for this VisionPortal.
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
+        myVisionPortalBuilder.setCamera(webcam);      // Other choices are: RC phone camera and "switchable camera name".
+
+        // Add the AprilTag Processor to the VisionPortal Builder.
+        myVisionPortalBuilder.addProcessor(threeRectangleProcessor);       // An added Processor is enabled by default.
+
+        // Optional: set other custom features of the VisionPortal (4 are shown here).
+        myVisionPortalBuilder.setCameraResolution(new Size(1280, 720));  // Each resolution, for each camera model, needs calibration values for good pose estimation.
+        myVisionPortalBuilder.setStreamFormat(VisionPortal.StreamFormat.YUY2);  // MJPEG format uses less bandwidth than the default YUY2.
+        myVisionPortalBuilder.enableLiveView(true);      // Enable LiveView (RC preview).
+        myVisionPortalBuilder.setAutoStopLiveView(true);     // Automatically stop LiveView (RC preview) when all vision processors are disabled.
+
+        // Create a VisionPortal by calling build()
+        myVisionPortal = myVisionPortalBuilder.build();
+
+        ThreeRectangleProcessor.Selected selection = ThreeRectangleProcessor.Selected.NONE;
+        while(!opModeIsActive()) {
+            selection = threeRectangleProcessor.getSelection();
+        }
+
+        waitForStart();
+
+        if(selection == ThreeRectangleProcessor.Selected.LEFT){
+            telemetry.addLine("It's left");
+        } else if(selection == ThreeRectangleProcessor.Selected.RIGHT){
+            telemetry.addLine("It's right");
+        } else if (selection == ThreeRectangleProcessor.Selected.MIDDLE){
+            telemetry.addLine("It's middle");
+        } else {
+            telemetry.addLine("Not Found");
+        }
+            telemetry.update();
+
+    }
+}
