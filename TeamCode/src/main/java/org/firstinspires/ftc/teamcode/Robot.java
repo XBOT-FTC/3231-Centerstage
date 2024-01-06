0aecf8adb04f66f0aa0a4c856bf9a14a8154aed0
Kurose <jackchenpetite@gmail.com>
2024-01-05 18:35:24 -0800

    Added drone shooter and called it in my main robot.java file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,20 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.graphics.Path;
-import android.nfc.cardemulation.HostNfcFService;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.internal.camera.libuvc.nativeobject.LibUsbDevice;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.Grabber;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.DroneShooter;
 
 @TeleOp(name="MD: Robot", group="Linear Opmode")
 public class Robot extends LinearOpMode {
@@ -26,8 +14,8 @@ public class Robot extends LinearOpMode {
         drive.setSpeedChange(0.25);
 
         //GRABBER SETUPS
-        Grabber grabber = new Grabber(hardwareMap);
-        grabber.setGrabPosition(1.0, 0.0);
+//        Grabber grabber = new Grabber(hardwareMap);
+//        grabber.setGrabPosition(1.0, 0.0);
 
         DroneShooter droneShooter = new DroneShooter(hardwareMap, Servo.Direction.FORWARD, 0.7, 0.1);
 
@@ -48,20 +36,24 @@ public class Robot extends LinearOpMode {
         hanger.setDownTicks(540);
         hanger.setHangerPower(1.0);
 
-        // INTAKER SETUPS
+
         Intaker intaker = new Intaker(hardwareMap);
-        intaker.setSpinPower(1.0);
+        intaker.setSpinPower(0.5);
         intaker.setStopPower(0.0);
+
+//       SNAPPER SETUPS
+        Snapper snapper = new Snapper(hardwareMap);
 
 
         waitForStart();
 
         while(opModeIsActive()) {
             drive.drive(gamepad1, telemetry);
-            grabber.grab(gamepad2, telemetry);
+//            grabber.grab(gamepad2, telemetry);
             intaker.moveIntake(gamepad2, telemetry);
             droneShooter.shootingControls(gamepad2, telemetry);
             hanger.moveHanger(gamepad2, telemetry);
+            snapper.moveSnapper(gamepad2, telemetry);
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Snapper.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Snapper {
+    private CRServo snapper = null;
+    private double correctPosition = 1.0;
+    private double currentLocation;
+
+    public Snapper(HardwareMap hardwareMap) {
+        snapper = hardwareMap.get(CRServo.class, "snapper");
+        snapper.setDirection(CRServo.Direction.FORWARD);
+    }
+
+    public void moveSnapper(Gamepad gamepad, Telemetry telemetry) {
+        snapperTrigger(gamepad, telemetry);
+    }
+
+    public void snapperTrigger(Gamepad gamepad, Telemetry telemetry) {
+            if (gamepad.right_bumper) {
+                snapper.setPower(1.0);
+            }
+            if (gamepad.left_bumper) {
+                snapper.setPower(0.0);
+            }
+
+            telemetry.addData("Snapper power:", snapper.getPower());
+
+    }
+
+}
